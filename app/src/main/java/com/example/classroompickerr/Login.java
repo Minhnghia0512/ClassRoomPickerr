4c8494af5dc1a36ef8762450c6e8b5534314b41a
Lê Thiên Phúc <55732482+prototype0162@users.noreply.github.com>
2020-12-29 17:40:50 +0700

    Login

--- app/src/main/java/com/example/classroompickerr/Login.java
+++ app/src/main/java/com/example/classroompickerr/Login.java
@@ -1,6 +1,5 @@
 package com.example.classroompickerr;
 
-import android.app.AppComponentFactory;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,32 +16,33 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
-    EditText txtEmail,txtPassword;
+    EditText tv_Email, tv_Pass;
     Button btn_Login;
     FirebaseAuth fAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        txtEmail = findViewById(R.id.txtEmail);
-        txtPassword = findViewById(R.id.txtPassword);
+        tv_Email = findViewById(R.id.tv_Email);
+        tv_Pass = findViewById(R.id.tv_Pass);
         fAuth = FirebaseAuth.getInstance();
         btn_Login = findViewById(R.id.btn_Login);
 
         btn_Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = txtEmail.getText().toString().trim();
-                String password = txtPassword.getText().toString().trim();
-                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                String email = tv_Email.getText().toString().trim();
+                String password = tv_Pass.getText().toString().trim();
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
-                            Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Logged in Successfully !", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
 
-                        }else {
-                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(Login.this, "Incorrect Email or Password !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
                     }
                 });
