0fcb30ab367adf665fad35ac68fc2f0b73679fe9
Lê Thiên Phúc <55732482+prototype0162@users.noreply.github.com>
2021-01-02 23:57:37 +0700

    Final

--- app/src/main/java/com/example/classroompickerr/Login.java
+++ app/src/main/java/com/example/classroompickerr/Login.java
@@ -2,12 +2,16 @@ package com.example.classroompickerr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,39 +20,56 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
-    EditText tv_Email, tv_Pass;
-    Button btn_Login;
+    EditText Email,Password;
+    Button Login;
     FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        tv_Email = findViewById(R.id.tv_Email);
-        tv_Pass = findViewById(R.id.tv_Pass);
-        fAuth = FirebaseAuth.getInstance();
-        btn_Login = findViewById(R.id.btn_Login);
 
-        btn_Login.setOnClickListener(new View.OnClickListener() {
+        Email = findViewById(R.id.txtemail);
+        Password = findViewById(R.id.txtpassword);
+        Login = findViewById(R.id.btn_Login);
+        progressBar = findViewById(R.id.progressBar);
+        fAuth = FirebaseAuth.getInstance();
+
+        Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = tv_Email.getText().toString().trim();
-                String password = tv_Pass.getText().toString().trim();
-                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                String email = Email.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Email.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Password.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    Password.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            Toast.makeText(Login.this, "Logged in Successfully !", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
                             startActivity(new Intent(getApplicationContext(), MainActivity.class));
-
                         } else {
-                            Toast.makeText(Login.this, "Incorrect Email or Password !" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
                         }
                     }
                 });
             }
         });
-
-
     }
 }
---
+++ app/src/main/java/com/example/classroompickerr/classroompickerr/Login.java
@@ -0,0 +1,75 @@
+package com.example.classroompickerr.classroompickerr;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.classroompickerr.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity {
+    EditText Email,Password;
+    Button Login;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Email = findViewById(R.id.txtemail);
+        Password = findViewById(R.id.txtpassword);
+        Login = findViewById(R.id.btn_Login);
+        progressBar = findViewById(R.id.progressBar);
+        fAuth = FirebaseAuth.getInstance();
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = Email.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Email.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Password.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    Password.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(com.example.classroompickerr.classroompickerr.Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(com.example.classroompickerr.classroompickerr.Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+    }
+}
