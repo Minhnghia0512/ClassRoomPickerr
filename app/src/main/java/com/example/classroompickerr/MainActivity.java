0070fe350f8d619ac72f5bffe6e9e48a68972d1e
187PM06626 - Cao Ngoc Lam - K24T-PM1 <lam.187pm06626@vanlanguni.vn>
2021-01-03 20:59:17 +0700

    Lam submit code ne
    Lam submit code hoi ne

--- app/src/main/java/com/example/classroompickerr/MainActivity.java
+++ app/src/main/java/com/example/classroompickerr/MainActivity.java
@@ -1,15 +1,61 @@
 package com.example.classroompickerr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.database.StaleDataException;
+import android.widget.ListView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-
+    FirebaseAuth fAuth;
+    FirebaseDatabase fDB;
+    DatabaseReference myRef;
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    String TAG="FIREBASE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+        adapter = new ArrayAdapter<>(this, android.R.layout.activity_list_item);
+        fDB = FirebaseDatabase.getInstance();
+        myRef = fDB.getReference("room");
+        listView = findViewById(R.id.ltviewroom);
+        listView.setAdapter(adapter);
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot datasnapshot) {
+                adapter.clear();
+                for (DataSnapshot data:datasnapshot.getChildren()){
+                    String key = data.getKey();
+                    String value = data.getValue().toString();
+                    adapter.add(key+"\n"+ value);
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d(TAG,"loadPost:onCancelled",databaseError.toException());
+            }
+        });
+
+    }
+
+    public void logout(View view){
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),Login.class));
+        finish();
     }
 }
