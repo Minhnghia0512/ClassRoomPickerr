8fc5069edfa2abfe18477dde8cb664dd8b0181e8
Nguyen Hieu <hieu.187pm20468@vanlanguni.vn>
2021-01-03 12:36:32 +0700

    Chỉnh sửa login

--- app/src/main/java/com/example/classroompickerr/Login.java
+++ app/src/main/java/com/example/classroompickerr/Login.java
@@ -1,14 +1,17 @@
 package com.example.classroompickerr;
 
-import android.app.AppComponentFactory;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,38 +20,56 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
-    EditText txtEmail,txtPassword;
-    Button btn_Login;
+    EditText Email,Password;
+    Button Login;
     FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        txtEmail = findViewById(R.id.txtEmail);
-        txtPassword = findViewById(R.id.txtPassword);
-        fAuth = FirebaseAuth.getInstance();
-        btn_Login = findViewById(R.id.btn_Login);
 
-        btn_Login.setOnClickListener(new View.OnClickListener() {
+        Email = findViewById(R.id.txtEmail);
+        Password = findViewById(R.id.txtPassword);
+        Login = findViewById(R.id.btn_Login);
+        progressBar = findViewById(R.id.progressBar);
+        fAuth = FirebaseAuth.getInstance();
+
+        Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String email = txtEmail.getText().toString().trim();
-                String password = txtPassword.getText().toString().trim();
+                String email = Email.getText().toString().trim();
+                String password = Password.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    Email.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    Password.setError("Password is Required.");
+                    return;
+                }
+
+                if(password.length() < 6){
+                    Password.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
                 fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(task.isSuccessful()){
+                        if (task.isSuccessful()) {
                             Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
-
-                        }else {
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
                             Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
                         }
                     }
                 });
             }
         });
-
-
     }
 }
